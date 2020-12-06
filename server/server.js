cf40ef5b1591a2aee66b531259056449e2e892e5
Carlos Barcos <fbarcos8@hotmail.com>
2020-12-06 03:51:35 -0500

    Configurando mongoose
    	modificado:     package-lock.json
    	modificado:     package.json
    	modificado:     server/server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('./config/config');
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
- 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+
 // parse application/x-www-form-urlencoded--> procesar peticiones form
 app.use(bodyParser.urlencoded({extended:false}));
 // parse application/json--> procesar peticiones json
@@ -35,9 +37,16 @@ app.put('/usuario/:id', function (req, res) {
     id
   });
 });
+
 app.delete('/usuario', function (req, res) {
   res.json('delete usuarios');
 });
+
+mongoose.connect(`mongodb://localhost:27017/cafe`,(err,res)=>{
+  if(err) throw err;
+  console.log('Base de datos ONLINE');
+});
+
 const port = process.env.PORT;
 app.listen(port,()=>{
   console.log(`Escuchando el puerto: ${port}`);
