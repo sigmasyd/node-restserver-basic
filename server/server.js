ec5e785cba93f456003a1ed2ba2cd62242958c78
Carlos Barcos <fbarcos8@hotmail.com>
2020-12-06 00:54:43 -0500

    Se crea servidor basico para manejar eventos api rest Se agrega express, body-parser y se devuelven estados de error
    	nuevo archivo:  .gitignore
    	nuevo archivo:  package-lock.json
    	nuevo archivo:  package.json
    	nuevo archivo:  server/server.js

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+ 
+// parse application/x-www-form-urlencoded--> procesar peticiones form
+app.use(bodyParser.urlencoded({extended:false}));
+// parse application/json--> procesar peticiones json
+app.use(bodyParser.json());
+
+app.get('/usuario', function (req, res) {
+  res.json('get usuarios');
+});
+
+// crear registro
+app.post('/usuario', function (req, res) {
+  let body = req.body;
+  if(body.nombre===undefined){
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    });
+  }else{
+    res.json({
+      persona:body
+    });
+  }
+});
+
+// actualizar registro
+app.put('/usuario/:id', function (req, res) {
+  let id = req.params.id;
+  res.json({
+    id
+  });
+});
+app.delete('/usuario', function (req, res) {
+  res.json('delete usuarios');
+});
+app.listen(3000,()=>{
+  console.log("Escuchando el puerto: ",3000);
+})
