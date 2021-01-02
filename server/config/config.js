a8536cc42ab35fb3f89e3c88d73efb81e1150899
Carlos Barcos <fbarcos8@hotmail.com>
2021-01-02 01:31:42 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev' ){
   urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-  urlDB = 'mongodb+srv://admin:eGXOJKlLydErPJH3@cluster0.solg7.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
