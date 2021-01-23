0e22e32ff40be7789f2ff3894e593546e6d01c98
Carlos Barcos <fbarcos8@hotmail.com>
2021-01-23 18:20:06 -0500

    Agregando jwt para el uso de tokens, al hacer login se valida y retorna el token.
    	modificado:     package-lock.json
    	modificado:     package.json
    	modificado:     server/config/config.js
    	nuevo archivo:  server/routes/index.js
    	nuevo archivo:  server/routes/login.js
    	modificado:     server/routes/usuario.js
    	modificado:     server/server.js

--- server/server.js
+++ server/server.js
@@ -6,28 +6,29 @@ const mongoose = require('mongoose');
 const app = express();
 
 // parse application/x-www-form-urlencoded--> procesar peticiones form
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json--> procesar peticiones json
 app.use(bodyParser.json());
 
-app.use( require('./routes/usuario') );
+// configuración global de rutas
+app.use(require('./routes/index'));
 
 //mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.URLDB,{
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  autoIndex: true,
-  dropDups: true
-})
-  .then((res)=>{
-    console.log('Base de datos ONLINE');
-  })
-  .catch((err)=>{
-    throw err;
-  });
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+        dropDups: true
+    })
+    .then((res) => {
+        console.log('Base de datos ONLINE');
+    })
+    .catch((err) => {
+        throw err;
+    });
 
 const port = process.env.PORT;
-app.listen(port,()=>{
-  console.log(`Escuchando el puerto: ${port}`);
+app.listen(port, () => {
+    console.log(`Escuchando el puerto: ${port}`);
 })
